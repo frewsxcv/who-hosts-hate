cfb66c0b8b28a523f0d83b7a7ebd6b7b11a0a6ce
Corey Farwell <coreyf@rwell.org>
2019-08-17 10:41:39 -0400

    add site rank, sort list

--- run.py
+++ run.py
@@ -3,10 +3,27 @@ import json
 import pprint
 import urllib.request
 import argparse
+import myawis
+import typing
+from xml.etree import ElementTree
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ipstack_access_key')
+parser.add_argument('aws_access_key_id')
+parser.add_argument('aws_secret_access_key')
 args = parser.parse_args()
+
+
+def site_rank(site: str) -> typing.Optional[int]:
+    obj = myawis.CallAwis(args.aws_access_key_id, args.aws_secret_access_key)
+    urlinfo = obj.urlinfo(site)
+    tree = ElementTree.fromstring(str(urlinfo))
+    rank = tree.findall(
+        './/aws:TrafficData/aws:Rank',
+        {'aws':"http://awis.amazonaws.com/doc/2005-07-11"}
+    )[0].text
+    # TODO fetch `aws:ContributingSubdomain`
+    return int(rank) if rank else None
 
 sites = [
     '4chan.org',
@@ -40,15 +57,15 @@ sites = [
     'nsm88.org',
     'officialproudboys.com',
     'patriotfront.us',
+    'politicalislam.com',
     'prageru.com',
     'profam.org',
     'stormfront.org',
     'tightroperecords.com',
     'understandingthethreat.com',
+    'unitedfamilies.org',
     'vachristian.org',
     'washsummit.com',
-    'politicalislam.com',
-    'unitedfamilies.org',
 ]
 
 isps = collections.defaultdict(lambda: [])
@@ -62,7 +79,9 @@ for site in sites:
 
     isp = response_json['connection']['isp']
 
-    isps[isp].append(site)
+    rank = site_rank(site)
+
+    isps[isp].append([site, rank])
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(dict(isps))
