8a121bfb9b4a83ecbdf15e29b41a781219220f0f
Corey Farwell <coreyf@rwell.org>
2021-01-20 14:30:24 -0500

    handle connection reset error

--- build.py
+++ build.py
@@ -147,6 +147,8 @@ class HateSiteLoader(typing.NamedTuple):
             return HateSiteErrorResponse(reason=str(error.reason), status_code=None)
         except TimeoutError as error:
             return HateSiteErrorResponse(reason="Timeout", status_code=None)
+        except ConnectionResetError as error:
+            return HateSiteErrorResponse(reason="Connection reset", status_code=None)
         return HateSiteResponse(body=response.read(), status_code=response.status)
 
 
